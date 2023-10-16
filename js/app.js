2203e30603204572958ea3bc3a74cab6d7b46b34
ayesharajput <143730428+ayesharajput@users.noreply.github.com>
2023-10-16 21:02:29 +0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,51 @@
+var hourHeading = document.getElementById("hour");
+var minHeading = document.getElementById("min");
+var secHeading = document.getElementById("sec");
+var msecHeading = document.getElementById("msec");
+
+
+var hour = 0;
+var min = 0;
+var sec = 0;
+var msec = 0;
+var pause = 0;
+var interval;
+
+function timer() {
+    msec++;
+    msecHeading.innerHTML = msec;
+    if (msec >= 100) {
+        sec++;
+        secHeading.innerHTML = sec;
+        msec = 0;
+    }
+    else if (sec >= 60) {
+        min++;
+        minHeading.innerHTML = min;
+    }
+    else if (min >= 60) {
+        hour++;
+        hourHeading.innerHTML = hour;
+    }
+}
+function start() {
+    interval = setInterval(timer, 10)
+}
+
+function pause() {
+    clearInterval(interval)
+}
+
+
+function reset() {
+    var hour = 0;
+    var min = 0;
+    var sec = 0;
+    var msec = 0;
+    msecHeading.innerHTML = msec;
+    secHeading.innerHTML = sec;
+    minHeading.innerHTML = min;
+    hourHeading.innerHTML = hour;
+    pause()
+
+}
